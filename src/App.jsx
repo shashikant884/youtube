83074cc383a4f6323df99456635e6cb3a74ad067
Shashikant <yadavshashikant884@gmail.com>
2025-04-18 13:00:55 +0530

    Hearder component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Head from "./componente/head";
 
 function App() {
   return (
     <div>
+      <Head />
       <h1 className="text-3xl font-bold underline">YOUTUBE React</h1>
     </div>
   );
